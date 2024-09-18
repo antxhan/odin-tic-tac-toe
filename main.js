ee17afe2b7d408780cab8bae1f2d3fdbe8b59cbd
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-09-18 19:24:15 +0200

    object tests

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-function createPlayer() {}
-
-function createGame() {}
+function createPlayer(name, marker) {
+  return { name, marker };
+}
 
 const gameboard = (function () {
-  const gameboard = [];
-  // return { gameboard };
+  const gameboard = ["", "", "", "", "", "", "", "", ""];
+  const getGameboard = () => gameboard;
+  const getSquare = (index) => gameboard[index];
+  const setGameboard = (index, marker) => (gameboard[index] = marker);
+  return { getGameboard, getSquare, setGameboard };
 })();
 
-const displayController = (function () {})();
+// Create players
+const player1 = createPlayer("john", "X");
+const player2 = createPlayer("jane", "O");
+
+let currentPlayer = player1;
+let currentPlayerIndex = prompt("Pick index");
+if (gameboard.getGameboard()[currentPlayerIndex] !== "") {
+  console.log("Square already occupied, pick different index");
+}
+gameboard.setGameboard(currentPlayerIndex, currentPlayer.marker);
+
+// Game:
+// Init: X plays first
+// If no empty string elements in array: game over.
+// Player: picks index of gameboard array to place their marker
+// If index is not empty string (already occupied), player needs to pick new index.
+// App checks if there's 3 in a row
+// If true: that player wins, else continue
+// Return to line 17 but with player 2
